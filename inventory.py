1f9236615eabcaef9622900d8067821abd33e7a3
srimad1111 <srimadsnehashis@gmail.com>
2026-07-05 11:28:26 +0530

    added ABC analysis based on revenue of the product

--- inventory.py
+++ inventory.py
@@ -14,3 +14,107 @@ def inventory_dashboard(df):
 
     st.markdown("---")
 
+    # Product Revenue
+
+    inventory = (
+
+        df.groupby("Description")["Revenue"]
+
+        .sum()
+
+        .sort_values(
+
+            ascending=False
+
+        )
+
+        .reset_index()
+
+    )
+
+    inventory.columns = [
+
+        "Product",
+
+        "Revenue"
+
+    ]
+
+    inventory["Revenue %"] = (
+
+        inventory["Revenue"] /
+
+        inventory["Revenue"].sum()
+
+    ) * 100
+
+    inventory["Cumulative %"] = (
+
+        inventory["Revenue %"]
+
+        .cumsum()
+
+    )
+
+    inventory["Category"] = "C"
+
+    inventory.loc[
+
+        inventory["Cumulative %"] <= 80,
+
+        "Category"
+
+    ] = "A"
+
+    inventory.loc[
+
+        (inventory["Cumulative %"] > 80) &
+        (inventory["Cumulative %"] <= 95),
+
+        "Category"
+
+    ] = "B"
+
+    st.subheader("📊 ABC Analysis")
+
+    fig = px.bar(
+
+        inventory.head(30),
+
+        x="Product",
+
+        y="Revenue",
+
+        color="Category",
+
+        color_discrete_map={
+
+            "A":"red",
+
+            "B":"orange",
+
+            "C":"green"
+
+        }
+
+    )
+
+    fig.update_layout(
+
+        template="plotly_white",
+
+        height=500,
+
+        xaxis_tickangle=-90
+
+    )
+
+    st.plotly_chart(
+
+        fig,
+
+        use_container_width=True
+
+    )
+
+    st.markdown("---")
