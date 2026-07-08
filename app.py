ec9d54894e6311f0635c23e7cc75a8059459337e
srimad1111 <srimadsnehashis@gmail.com>
2026-07-08 19:08:41 +0530

    added footer and loading animation

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 from churn import churn_dashboard
-
 from utils import load_data, clean_data
-
 from executive import executive_dashboard
 from inventory import inventory_dashboard
 from sales import sales_dashboard
 from customer import customer_dashboard
 from forecast import forecast_dashboard
+ 
 
-
-# Streamlit Configuration
 st.set_page_config(
     page_title="NeuralRetail AI",
     page_icon="🛒",
@@ -18,35 +15,25 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-import streamlit as st
-
-st.set_page_config(page_title="NeuralRetail AI", layout="wide")
-
-
-
-# Load Dataset
-df = load_data()
-df = clean_data(df)
-
-
-# Sidebar
+with st.spinner("Loading Dashboard..."):
+    df = load_data()
+    df = clean_data(df)
 
 st.sidebar.image(
     "assets/logo.png",
-    use_container_width=True
+    width=180     
 )
 
 st.sidebar.title("NeuralRetail AI")
 st.sidebar.caption("Retail Intelligence Platform")
 
-
 PAGES = {
-    " Executive Overview": executive_dashboard,
-    " Sales Analytics": sales_dashboard,
-    " Customer Hub": customer_dashboard,
-    " Demand Explorer": forecast_dashboard,
-    " Churn Prediction": churn_dashboard,
-    " Inventory Optimization": inventory_dashboard,
+    "Executive Overview": executive_dashboard,
+    "Sales Analytics": sales_dashboard,
+    "Customer Hub": customer_dashboard,
+    "Demand Explorer": forecast_dashboard,
+    "Churn Prediction": churn_dashboard,
+    "Inventory Optimization": inventory_dashboard,
 }
 
 page = st.sidebar.radio(
@@ -55,28 +42,18 @@ page = st.sidebar.radio(
 )
 
 st.sidebar.markdown("---")
-
-st.sidebar.subheader("Current Modules")
-st.sidebar.success("✔ Executive Dashboard")
-st.sidebar.success("✔ Sales Analytics")
-st.sidebar.success("✔ Customer Segmentation")
-st.sidebar.success("✔ Demand Forecasting")
-
-st.sidebar.markdown("---")
-
-st.sidebar.subheader("Upcoming Modules")
-st.sidebar.info("📌 Churn Prediction")
-st.sidebar.info("📌 Inventory Optimization")
-
-st.sidebar.markdown("---")
 st.sidebar.caption("NeuralRetail AI v1.0")
 
 
-# Main Title
 st.title("🛒 NeuralRetail AI")
 st.caption("AI Powered Retail Sales Intelligence Dashboard")
 st.markdown("---")
 
-
-# Navigation
+# Open selected page
 PAGES[page](df)
+
+st.markdown("---")
+
+st.caption(
+    "© 2026 NeuralRetail AI | By Srimad Snehashis | Made with ❤️"
+)
